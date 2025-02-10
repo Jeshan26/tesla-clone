3cf539e67d744a9aa60c113c9d375a6a03a38867
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-02-10 15:23:25 -0500

    Added reduc store for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { FaTimes, FaBars } from 'react-icons/fa';
+import {selectCars} from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
-// import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
-  return (
+const [burgerStatus , setBurgerStatus ] = useState(false);
+const cars = useSelector(selectCars);
+console.log(cars);
+
+return (
     <Container>
       <a>
         <img src='/images/logo.svg' alt='Logo' />
       </a>
       <Menu>
-
-        <a href='#'>Model S</a>
-        <a href='#'>Model 3</a>
-        <a href='#'>Model X</a>
-        <a href='#'>Model Y</a>
-
+        {cars && cars.map((car,index)=>(
+          <li key={index} ><a href='#'>{car}</a></li>
+        ))}
       </Menu>
 
       <RightMenu>
       <a href='#'>Shop</a>
       <a href='#'>Tesla Account</a>
-      <CustomMenu />
+      <CustomMenu onClick={ () => setBurgerStatus(true)}  />
       </RightMenu>
+      <BurgerNav show={burgerStatus}> 
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)}/>
+        </CloseWrapper>
+        {cars && cars.map((car,index)=>(
+          <li key={index}><a  href='#'>{car}</a></li>
+        ))}
+        <li><a href='#'>Existing inventory</a></li>
+        <li><a href='#'>Used inventory</a></li>
+        <li><a href='#'>Trade-In</a></li>
+        <li><a href='#'>Cyber Truck</a></li>
+        <li><a href='#'>Discover</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><a href='#'>Support</a></li>
+      </BurgerNav>
+
     </Container>
   )
 }
@@ -39,13 +58,14 @@ top :0;
 left:0;
 right : 0;
 padding: 0 20px;
-
+z-index : 1;
 `
 const Menu = styled.div`
   display :flex;
   align-items: center;
   flex : 1;
   justify-content : center;
+  list-style : none;
 
   a {
     font-weight : 600;
@@ -66,9 +86,49 @@ a {
     margin-right : 10px;
   }
 
+display:flex;
+`
+
+const CustomMenu = styled(FaBars)`
+cursor : pointer;
 
 `
 
-const CustomMenu = styled.div`
+const BurgerNav = styled.div`
+position : fixed;
+top: 0;
+bottom :0;
+right: 0;
+background-color : white;
+width : 300px;
+z-index : 200;
+list-style : none;
+padding : 20px;
+display : flex;
+flex-direction : column;
+text-align : start;
 
+transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'  };
+
+transition : transform 0.2s;
+
+li{
+  padding : 15px 0;
+  border-bottom : 1px solid rgba(0,0,0,0.2);
+
+  a {
+    font-weight : 600;
+  }
+
+}
+
+`
+const CustomClose = styled(FaTimes)`
+cursor: pointer;
+
+`
+
+const CloseWrapper = styled.div`
+display : flex;
+justify-content : flex-end;
 `
